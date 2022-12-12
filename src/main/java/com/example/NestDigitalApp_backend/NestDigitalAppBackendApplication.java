431c43fc5359de8f48bc6f24e395d5e01d373f43
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-12 11:56:57 +0530

    first commit

---
+++ src/main/java/com/example/NestDigitalApp_backend/NestDigitalAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestDigitalApp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestDigitalAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestDigitalAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NestDigitalApp_backend/NestDigitalAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NestDigitalApp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestDigitalAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
