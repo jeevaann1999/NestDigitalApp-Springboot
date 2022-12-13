ff0678d823ef4ccd1b345ea8b5337e09e08a3e5a
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-13 09:39:47 +0530

    leave apply

--- src/main/java/com/example/NestDigitalApp_backend/controller/LeaveApplicationController.java
+++ src/main/java/com/example/NestDigitalApp_backend/controller/LeaveApplicationController.java
@@ -1,4 +1,28 @@
 package com.example.NestDigitalApp_backend.controller;
 
+import com.example.NestDigitalApp_backend.dao.LeaveApplicationDao;
+import com.example.NestDigitalApp_backend.model.LeaveApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+@RestController
 public class LeaveApplicationController {
+    @Autowired
+    private LeaveApplicationDao dao;
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path ="/leaveapply",produces = "application/json",consumes = "application/json")
+    public Map<String,String> ApplyLeave(@RequestBody LeaveApplication l) {
+        dao.save(l);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "success");
+        return map;
+    }
+
+
 }
--- src/main/java/com/example/NestDigitalApp_backend/model/EmployeeLog.java
+++ src/main/java/com/example/NestDigitalApp_backend/model/EmployeeLog.java
@@ -1,12 +1,16 @@
 package com.example.NestDigitalApp_backend.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "employeeLog")
 public class EmployeeLog {
 
+    @Id
+    @GeneratedValue
     private int id;
     private  String empcode;
     private String date;
